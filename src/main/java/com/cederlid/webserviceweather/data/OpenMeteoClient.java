d0f56b5e668886f70684d1a6c6ba1c41e8bdd76f
Wijdan Cederlid <wijdan.cederlid@iths.se>
2023-05-19 15:10:29 +0200

    Getting the correct hour in OpenMeteoClient but there is more fixes that I have to do

--- src/main/java/com/cederlid/webserviceweather/data/OpenMeteoClient.java
+++ src/main/java/com/cederlid/webserviceweather/data/OpenMeteoClient.java
@@ -1,10 +1,14 @@
 package com.cederlid.webserviceweather.data;
 
 import com.cederlid.webserviceweather.buisness.Weather;
+import com.cederlid.webserviceweather.data.openMeteo.Hourly;
 import com.cederlid.webserviceweather.data.openMeteo.OpenMeteoWeather;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Repository
 public class OpenMeteoClient {
@@ -17,12 +21,24 @@ public class OpenMeteoClient {
                 .retrieve()
                 .bodyToMono(OpenMeteoWeather.class);
         OpenMeteoWeather weather = m.block();
-
         assert weather != null;
-        double temp = weather.getHourly().getTemperature2m().get(37);
-        double humid = weather.getHourly().getRelativehumidity2m().get(37);
-        String time = weather.getHourly().getTime().get(37);
-        return new Weather(temp, humid, "Open meteo", time);
+        String time;
+        Hourly hourly = weather.getHourly();
+        int hourIndex = -1;
+        for (int i = 0; i < hourly.getTime().size() ; i++) {
+            if (Integer.parseInt(hourly.getTime().get(i).substring(11, 13)) == (LocalTime.now().getHour()) && Integer.parseInt(hourly.getTime().get(i).substring(8,10)) == (LocalDate.now().getDayOfMonth() + 1) ) {
+                hourIndex = i;
+            }
+        }
+        if(hourIndex >= 0){
+            double temp = weather.getHourly().getTemperature2m().get(hourIndex);
+            double humid = weather.getHourly().getRelativehumidity2m().get(hourIndex);
+            time = weather.getHourly().getTime().get(hourIndex);
+            return new Weather(temp, humid, "Open meteo", time);
+        }
+        else {
+            throw new RuntimeException("Couldn't find hour!");
+        }
 
     }
 }
